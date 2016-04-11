1b195c599f9a6d49eaa9579fcd3b179056c793dc
Louise Knudsen <louise-w-knudsen@hotmail.com>
2016-04-11 13:00:51 +0200

    test fixed

--- src/test.py
+++ src/test.py
@@ -1,7 +1,9 @@
 import parser
 import segmentation
 import cv2
-img = parser.get_image('../data/leafscan_selection/', '100142.jpg')
-#img[0][0] = [0, 0, 0]
+img = parser.get_image('../../Data/leg/', '100142.jpg')
+img[0][0] = [0, 0, 0]
 cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 #print(img)
