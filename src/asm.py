4324bc7d3189e5d182f912f85c042669100be132
Louise Knudsen <louise-w-knudsen@hotmail.com>
2016-04-13 16:32:29 +0200

    table module added, aligner done

--- src/asm.py
+++ src/asm.py
@@ -1,21 +1,22 @@
 import cv2
 import numpy as np
 import math
+from table import image_table
 
 # Compute the V_R_kl of all distances
 # argument: image_table of all img_structs
 # returns: n times n matirx of variances, where n is #landmarks
-def compute_var_dist(image_table):
-
-    matrix_stack = []
+def compute_var_dist():
+    # number of (x,y)'s
+    n = len(image_table[0]['landmarks'])/2
+    # init dummy array to stack upon
+    matrix_stack = np.ndarray((n,n))
 
     for img in image_table:
         # (x,y)*n --> [x_0, y_0, x_1, ... , x_n-1, y_n-1]
         shape = img['landmarks']
-        # number of (x,y)'s
-        n = len(shape)/2
         # create empty n x n matrix to hold distances
-        dist_matrix = np.zeros(n,n)
+        dist_matrix = np.ndarray((n,n))
         for i in range(0,n):
             # the element from which the distance is measured
             k = np.array(shape[i*2], shape[i*2+1])
@@ -24,15 +25,18 @@ def compute_var_dist(image_table):
                 l = np.array(shape[j*2], shape[j*2+1])
                 # the distance from k to l
                 dist_matrix[i][j] = np.linalg.norm(k-l)
-                # stack each dist_matrix 'on top' of eachother
-                matrix_stack = np.dstack(matrix_stack, dist_matrix)
+        # stack each dist_matrix 'on top' of eachother
+        matrix_stack = np.dstack((matrix_stack, dist_matrix))
+    # remove the dummy array
+    matrix_stack = np.delete(matrix_stack, 0, axis=2)
 
-    m = len(matrix_stack[0])
-    var_matrix = np.zeros(m,m)
+    # init matrix of variances with uninitialized values
+    var_matrix = np.ndarray((n,n))
 
     # calculate the variance of each point distance
     for i in range(0, m):
         for j in range(0, m):
+            # find the variance of each point
             var_matrix[i][j] = np.var(matrix_stack[i][j])
 
     return var_matrix
@@ -49,26 +53,20 @@ def k_weight(var_matrix, k):
 # arguments: the i'th shape, V_R_kl matrix
 # returns: X_i
 def x_sum(shape, var_matrix):
-    # get all the x-coordinates of the shape
-    for i in range(0, len(shape)/2):
-        xses[i] = shape[i*2]
     summ = 0
-    for j in range(0,len(xes)):
-        weight = k_weight(var_matrix, j)
-        summ += weight * xses[j]
+    for i in range(0,len(shape)/2):
+        weight = k_weight(var_matrix, i)
+        summ += weight * shape[i*2]
     return summ
 
 # Compute Y_i = sum^n-1_k=0 w_k * y_ik (31)
 # arguments: the i'th shape, V_R_kl matrix
 # returns: Y_i
 def y_sum(shape, var_matrix):
-    # get all the y-coordinates of the shape
-    for i in range(0, len(shape)/2):
-        yses[i] = shape[i*2+1]
     summ = 0
-    for j in range(0,len(yses)):
-        weight = k_weight(var_matrix, j)
-        summ += weight * yses[j]
+    for i in range(0,len(shape)/2):
+        weight = k_weight(var_matrix, i)
+        summ += weight * shape[i*2+1]
     return summ
 
 # Compute Z = sum^n-1_k=0 w_k (x^2_nk + y^2_nk) (32)
@@ -153,4 +151,69 @@ def solve_x(shape1, shape2, var_matrix):
 
     return x
 
-def align_pair()
+# Align shape2 (x_2) by mapping x_2 onto M(x_2)+t
+# arguments: the shape to be aligned, x vector containing s, theta and (t_x, t_y)
+# returns: The aligned shape
+def align_pair(shape2, x):
+    a_x = x[0] # s cos theta
+    a_y = x[1] # s sin theta
+    t_x = x[2]
+    t_y = x[3]
+
+    n = len(shape2)/2
+    t = np.array([t_x,t_y])
+    # create vector t (translation) of same lenght as shape2
+    # with t_x, t_y repeated
+    t = np.tile(t, n)
+    # initiate a vector of same dimentions and values as shape2
+    M = np.copy(shape2)
+    # rotate and scale shape2 (now know as M)
+    for k in range(0,n):
+        M[k*2]   = (a_x * M[k*2]) - (a_y * M[k*2+1])
+        M[k*2+1] = (a_y * M[k*2]) - (a_x * M[k*2+1])
+    # translate by t
+    M = M + t
+    return M
+
+# Align all shapes with the given shape
+# arguments: the image table with all image dicts, the shape with which all
+#            others are to be aligned
+# returns: var_matrix (kind of a hack)
+def align_all_shapes(mean_shape):
+    var_matrix = compute_var_dist()
+    for img in image_table:
+        shape = img['landmarks']
+        x = solve_x(mean_shape, shape, var_matrix)
+        img['landmark'] = align_pair(shape, x)
+    return var_matrix
+
+# Calculate the mean shape from the aligned shapes
+# argument: the image table with all image dicts
+# returns: the mean shaoe
+def mean_shape():
+    shape_stack = image_table[0]['landmarks']
+    for img in image_table[1:]:
+        shape = img['landmarks']
+        shape_stack = np.dstack((shape_stack, shape))
+
+    shape_length = len(shape_stack[0])
+    mean_shape = np.zeros(shape_length)
+    for i in range(0, shape_length):
+        mean_shape[i] = np.mean(shape_stack[0][i])
+
+    return mean_shape
+
+def normalize_mean(shape1, mean_shape, var_matrix):
+    x = solve_x(shape1, mean_shape, var_matrix)
+    norm_mean = align_pair(mean_shape, x)
+    return norm_mean
+
+def the_real_aligner():
+    # the first shape is saved to be used for normalizing mean
+    shape1 = image_table[0]['landmarks']
+    # rotate, scale and translate each shape to align with the first shape
+    var_matrix = align_all_shapes(shape1)
+    while not converges: # JA det er lort!
+        mean_shape = mean_shape()
+        new_mean = normalize_mean(shape1, mean_shape, var_matrix)
+        align_all_shapes(new_mean)
