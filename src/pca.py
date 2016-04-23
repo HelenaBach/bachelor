e65598555996a5dc69d8ae5882146fd813550043
Helena Bach <hfb@quantads.net>
2016-04-23 10:29:16 +0200

    pca wrapper implemented

--- src/pca.py
+++ src/pca.py
@@ -1,8 +1,12 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
+#data = np.array([[-1, -1, -1, -1], [-2, -1, -2, -4], [-3, -2, -2, -2], [1, 1, 4, 2], [2, 1, 3, 5], [3, 2, 1, 3]])
 
-X = np.array([[-1, -1, -1, -1], [-2, -1, -2, -4], [-3, -2, -2, -2], [1, 1, 4, 2], [2, 1, 3, 5], [3, 2, 1, 3]])
-pca = PCA(n_components=0.95) #'mle' eller 0.952½
-pca.fit(X)
-print(pca.explained_variance_ratio_)
+def train_get_components(data, acc):
+	pca = PCA(n_components=acc)#0.95) #'mle' eller 2
+	pca.fit(data)
+	return pca.components_
+
+	#print(pca.explained_variance_ratio_)
+	#print(pca.components_)
